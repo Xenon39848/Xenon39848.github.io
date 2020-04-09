bc104ab69d8c02f272be7ba1ef47cba80a92f318
Xenon39848 <47875182+Xenon39848@users.noreply.github.com>
2020-04-09 03:39:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+
 <?php
-header('Access-Control-Allow-Origin: https://www.roblox.com');
-header('Access-Control-Allow-Credentials: true');
-?>
+echo '{"access_token":"tcaddxplvhjtvlxh6lmckivqqfdamxputnculgc4","expires_in":3600,"token_type":"Bearer","scope":"basic","refresh_token":"ba6cb6r2g2leh3a8nmxvnhimnukqdmupdyxuzqlu"}';
+
+ if( (isset($_GET["access_token"]))){
+ echo '{"user_id":"50794","reason":3,"current_app_version":123,"user_registered":"2020-03-23 17:20:38","trial_end_date":"2020-04-06","is_end_trial":true,"trial_duration_min":10}';
+ }
+ else if( (isset($_GET["oauth"]))){
+echo '{"access_token":"tcaddxplvhjtvlxh6lmckivqqfdamxputnculgc4","expires_in":3600,"token_type":"Bearer","scope":"basic","refresh_token":"ba6cb6r2g2leh3a8nmxvnhimnukqdmupdyxuzqlu"}';
+}
+    ?>
